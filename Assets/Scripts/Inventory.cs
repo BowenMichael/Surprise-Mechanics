57219134e72420e6e83681abc840dddc6b6c0bdd
Michael Bowen <michael.bowen@mymail.champlain.edu>
2020-02-12 17:53:33 -0500

    Added inventory functionality

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    
+    public GameObject inventory;
+    public GameObject slotHolder;
+
+    private bool inventoryIsOn = false;
+    private GameObject[] slots;
+
+    void Start()
+    {
+        slots = GameObject.FindGameObjectsWithTag("Slot");
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryIsOn = !inventoryIsOn;
+        }
+       
+        if (inventoryIsOn)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+    }
+}
